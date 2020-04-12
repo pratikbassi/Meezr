7c7d4472821fc397f381b1846dc9e61f6a7cd122
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-04-12 02:26:41 +0000

    added two migrations and tables to database

--- db/migrate/20200411224102_create_users.rb
+++ db/migrate/20200411224102_create_users.rb
@@ -3,11 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :user_name
       t.string :email 
-      t.string :password 
+      t.string :user_password 
       t.integer :bmi
 
-
-      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_224102) do
+ActiveRecord::Schema.define(version: 2020_04_12_013902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -36,13 +36,19 @@ ActiveRecord::Schema.define(version: 2020_04_11_224102) do
   enable_extension "uuid-ossp"
   enable_extension "xml2"
 
+  create_table "meals", force: :cascade do |t|
+    t.bigint "user_id"
+    t.boolean "is_public", default: true
+    t.boolean "is_deleted", default: false
+    t.index ["user_id"], name: "index_meals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "email"
-    t.string "password"
+    t.string "user_password"
     t.integer "bmi"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "meals", "users"
 end
