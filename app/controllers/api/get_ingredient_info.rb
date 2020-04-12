d8a12d8dcc8feb98abf265c525760477f332a48f
Bobby Uno <robert.y.uno@gmail.com>
2020-04-12 23:57:45 +0000

    get_ingredient_info returns all of its nutritional info

---
+++ app/controllers/api/get_ingredient_info.rb
@@ -0,0 +1,34 @@
+require 'faraday'
+require 'json'
+require 'dotenv'
+Dotenv.load
+
+# Retrieves all the information an ingredient has. Parameters id is a strind, portions_grams is a number.
+class IngredientsInfo
+
+  attr_accessor :id, :portion_grams
+
+  def initialize(id, portion_grams)
+    self.id = id
+    self.portion_grams = portion_grams
+  end
+
+  def get_ingredient_info
+    connection = Faraday.new(
+      url: 'https://api.spoonacular.com/food/ingredients/',
+      params: {apiKey: ENV['API_KEY']},
+    )
+
+    response = connection.get(self.id+'/information') do |request|
+      request.params['amount'] = self.portion_grams
+      request.params['unit'] = 'grams'
+      # req.body = {query: 'butter'}.to_json # would be used for like parseIngredients API i think
+    end
+
+    data = JSON.parse(response.body)
+    puts JSON.pretty_generate(data)
+  end
+end
+
+pineapple_info = IngredientsInfo.new('9266', 100)
+pineapple_info.get_ingredient_info
