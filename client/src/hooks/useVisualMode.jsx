1f156cbb8dee3a498ce08f54667f723ec28d9970
forgehe <liao098@gmail.com>
2020-04-15 21:40:39 -0700

    change history to historyVM to avoid name conflict

--- client/src/hooks/useVisualMode.jsx
+++ client/src/hooks/useVisualMode.jsx
@@ -2,23 +2,24 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
+  const [historyVM, setHistoryVM] = useState([initial]);
 
   const transition = (newMode, replace = false) => {
     setMode(newMode);
     if (replace) {
-      setHistory((prevHistory) => [...prevHistory.slice(0, -1), newMode]);
+      setHistoryVM((prevHistory) => [...prevHistory.slice(0, -1), newMode]);
     } else {
-      setHistory((prevHistory) => [...prevHistory, newMode]);
+      setHistoryVM((prevHistory) => [...prevHistory, newMode]);
     }
+    console.log("VM", mode, historyVM);
   };
 
   const back = () => {
-    if (history.length > 1) {
-      setMode(history[history.length - 2]);
-      setHistory((prevHistory) => prevHistory.slice(0, -1));
+    if (historyVM.length > 1) {
+      setMode(historyVM[historyVM.length - 2]);
+      setHistoryVM((prevHistory) => prevHistory.slice(0, -1));
     }
   };
 
-  return { mode, transition, back };
+  return { mode, transition, back, historyVM };
 }
