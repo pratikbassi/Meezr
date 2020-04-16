ab75441d63db2041e04b090feb596095fa7f004e
Bobby Uno <robert.y.uno@gmail.com>
2020-04-16 03:38:31 +0000

    tags/diets can be retrieved by recipe id

--- app/services/get_tags.rb
+++ app/services/get_tags.rb
@@ -3,14 +3,16 @@ require 'json'
 require 'dotenv'
 Dotenv.load
 
-# Retrieves all the information an ingredient has. Parameters id is a strind, portions_grams is a number.
+# Searches for meal/recipe by id and then return the diets/tags from the body
+
 class GetTags
 
-  attr_accessor :id, :include_nutrtion
+  attr_accessor :id, :include_nutrtion, :tags_obj
 
   def initialize(id)
     self.id = id
     self.include_nutrtion = false
+    self.tags_obj = {}
   end
 
   def get_tags
@@ -20,18 +22,37 @@ class GetTags
     )
 
     response = connection.get(self.id+'/information') do |request|
-      request.params['id'] = self.id
       request.params['includeNutrition'] = self.include_nutrtion
     end
 
     return nil if response.status != 200
   
-    data = JSON.parse(response.body)
-    data["vegetarian"]
+    # pp data = JSON.parse(response.body)
     
+    if(data["vegetarian"])
+      tags_obj[:vegetarian] = "Vegetarian"
+    end
+    if(data["vegan"])
+      tags_obj[:vegan] = "Vegan"
+    end
+    if (data["glutenFree"])
+      tags_obj[:glutenFree] = "Gluten Free"
+    end
+    if(data["dairyFree"])
+      tags_obj[:dairyFree] = "Dairy Free"
+    end
+    if(data["whole30"])
+      tags_obj[:whole30] = "Whole 30"
+    end
+    if(data["ketogenic"])
+      tags_obj[:ketogenic] = "Ketogenic"
+    end
+
+    #object/hash contain all of the tags that returned true. 
+    tags_obj
   end
 end
 
-tags = GetTags.new('716429')
-pp tags.get_tags
+# tags = GetTags.new('1003464') #1003464, 716429
+# pp tags.get_tags
 
