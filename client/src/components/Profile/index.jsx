0e6e979ccca27f25a290b9f1aab03c5b3b60f4dd
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-04-20 03:42:54 +0000

    created meal list and meal list item props

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
-// import { } from "@material-ui/icons";
+import Cookies from "js-cookie";
+import axios from "axios";
+
+
+
 
 const useStyles = makeStyles({});
 
 export default function Profile() {
+  const [state, useState] = React.useState()
+  const user = Cookies.get('user_id')
+
+
+  const getMealsForUser = (user) => {
+    return Promise.resolve(axios.)
+  }
+
   const classes = useStyles();
   return (
     <div>
-      this is the profiles page
-      <p>this is a p tag</p>
+      <MealList info={[]} />
     </div>
   );
 }
