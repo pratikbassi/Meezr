aa7ea929b9cf2afd66d1ad6be64f10ed4321943a
forgehe <liao098@gmail.com>
2020-04-20 20:11:25 -0700

    update component imports fix

--- client/src/components/Profile/MealList.jsx
+++ client/src/components/Profile/MealList.jsx
@@ -3,36 +3,42 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "theme";
 
-import MealListItem from "../Profile/MealListItem"
+import MealListItem from "../Profile/MealListItem";
 import { Typography } from "@material-ui/core";
 
 export default function MealList(props) {
-  const [state, useState] = React.useState(null)
-  const strings = ["Aren't you hungry?", "Add some meals!", "Meez'r is based on 'Mise en place'!", "It's a good day to make a meal!", "Time to break out the cookbook!"]
+  const [state, useState] = React.useState(null);
+  const strings = [
+    "Aren't you hungry?",
+    "Add some meals!",
+    "Meez'r is based on 'Mise en place'!",
+    "It's a good day to make a meal!",
+    "Time to break out the cookbook!",
+  ];
 
   function CheckList() {
-    if(props.info) {
+    if (props.info) {
       return (
         <ThemeProvider theme={theme}>
           {props.info.map((item) => {
-            <MealListItem mealPhoto={item.mealPhoto} mealTitle={item.mealTitle} mealId={item.mealId}/>
+            return (
+              <MealListItem
+                mealPhoto={item.mealPhoto}
+                mealTitle={item.mealTitle}
+                mealId={item.mealId}
+              />
+            );
           })}
         </ThemeProvider>
-      )
+      );
     } else {
       return (
         <ThemeProvider theme={theme}>
-          <Typography  variant="h5">
-            {strings.sample}
-          </Typography>
+          <Typography variant="h5">{strings.sample}</Typography>
         </ThemeProvider>
-
-      )
+      );
     }
-      
   }
 
-  return (
-      <CheckList/>
-  )
+  return <CheckList />;
 }
